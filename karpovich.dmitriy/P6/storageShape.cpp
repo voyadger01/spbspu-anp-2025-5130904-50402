82916101caa275bd38b1221c4d2ea7bb4172d022
voyadger01 <voyadgerkx@gmail.com>
2026-01-07 15:45:03 +0300

    add smth to cpp file

--- karpovich.dmitriy/P6/storageShape.cpp
+++ karpovich.dmitriy/P6/storageShape.cpp
@@ -1,1 +1,97 @@
 #include "storageShape.hpp"
+karpovich::storageShape::storageShape():
+  shapes_ (nullptr),
+  size_(0)
+{}
+double karpovich::storageShape::getArea() const noexcept
+{
+  size_t total = 0;
+  for (size_t i = 0; i < size_; ++i) {
+    total += shapes_[i]->getArea();
+  }
+}
+karpovich::rectangle_t karpovich::storageShape::getFrameRect() const noexcept
+{
+  
+}
+void karpovich::storageShape::move(point_t p) noexcept
+{
+
+}
+void karpovich::storageShape::move(double dx, double dy) noexcept
+{
+
+}
+
+void karpovich::storageShape::append(const Shape* app)
+{
+
+}
+void karpovich::storageShape::preappend(const Shape* app)
+{
+
+}
+void karpovich::storageShape::add(const Shape* app, size_t idx)
+{
+
+}
+karpovich::Shape& karpovich::storageShape::first() const
+{
+
+}
+karpovich::Shape& karpovich::storageShape::last() const
+{
+
+}
+const karpovich::Shape& karpovich::storageShape::firstConst() const
+{
+
+}
+const karpovich::Shape& karpovich::storageShape::lastConst() const
+{
+
+}
+karpovich::Shape& karpovich::storageShape::at(size_t idx) const
+{
+
+}
+karpovich::Shape& karpovich::storageShape::get(size_t idx) const
+{
+
+}
+const karpovich::Shape& karpovich::storageShape::atConst(size_t idx) const
+{
+
+}
+const karpovich::Shape& karpovich::storageShape::getConst(size_t idx) const
+{
+
+}
+void karpovich::storageShape::remove(size_t k)
+{
+
+}
+void karpovich::storageShape::dropFirst()
+{
+
+}
+void karpovich::storageShape::dropLast()
+{
+
+}
+void karpovich::storageShape::clear()
+{
+
+}
+size_t karpovich::storageShape::size()
+{
+
+}
+bool karpovich::storageShape::empty()
+{
+
+}
+karpovich::storageShape::~storageShape()
+{
+
+}
--- karpovich.dmitriy/P6/storageShape.hpp
+++ karpovich.dmitriy/P6/storageShape.hpp
@@ -11,26 +11,29 @@ namespace karpovich
     void move(point_t p) noexcept override;
     void move(double dx, double dy) noexcept override;
 
-    void append(Shape app);
-    void preappend(Shape app);
-    void add(Shape app, size_t idx);
-    Shape* first() const;
-    Shape* last() const;
-    const Shape* firstConst() const;
-    const Shape* lastConst() const;
-    Shape* at(size_t idx);
-    Shape* get(size_t idx);
-    const Shape* atConst(size_t idx);
-    const Shape* getConst(size_t idx);
+    void append(const Shape* app);
+    void preappend(const Shape* app);
+    void add(const Shape* app, size_t idx);
+    Shape& first() const;
+    Shape& last() const;
+    const Shape& firstConst() const;
+    const Shape& lastConst() const;
+    Shape& at(size_t idx) const;
+    Shape& get(size_t idx) const;
+    const Shape& atConst(size_t idx) const;
+    const Shape& getConst(size_t idx) const;
     void remove(size_t k);
     void dropFirst();
     void dropLast();
     void clear();
-    void size();
+    size_t size();
     bool empty();
     ~storageShape();
+
     private:
       void doScale(double k) noexcept override;
+      Shape** shapes_;
+      size_t size_;
   };
 }
 #endif
